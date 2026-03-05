e126e1e60ec81aaec0423e62b0d5f3d475f9dcfa
Sahed-Khan <sahed5000a@gmail.com>
2026-03-05 12:23:58 +0100

    premier commit ajout de index.html style.css et script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showPage(pageId){
+
+let pages = document.querySelectorAll(".page")
+
+pages.forEach(page =>{
+page.classList.remove("active")
+})
+
+document.getElementById(pageId).classList.add("active")
+
+}
